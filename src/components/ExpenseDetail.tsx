b98c6c3d0ef1a559a56feab290b9de3032c81ff7
deivis herrera <herrera.15cortes@gmail.com>
2025-08-26 09:01:28 -0500

    The component to display expenses was created.

---
+++ src/components/ExpenseDetail.tsx
@@ -0,0 +1,13 @@
+import type { Expense } from "../types"
+
+type ExpenseDetailProps = {
+    expense: Expense
+}
+
+export default function ExpenseDetail({expense} : ExpenseDetailProps) { //lo asignamos
+  return (
+    <div>
+      
+    </div>
+  )
+}
