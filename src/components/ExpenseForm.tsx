d41d1e9b8f39afbaa2cdacec8876507a22a8c3ca
deivis herrera <herrera.15cortes@gmail.com>
2025-08-26 11:37:55 -0500

    edit spending

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -55,8 +55,12 @@ export default function ExpenseForm() {
             return
         }
         
-        //Agregar gasto
-        dispatch({type : "add-expense", payload: {expense}}) 
+        //Agregar o Actualizar el  gasto
+        if(state.editingId) {
+            dispatch({type: 'update-extense', payload: {expense: { id: state.editingId, ...expense}}})
+        } else {
+            dispatch({type : "add-expense", payload: {expense}}) 
+        }
 
         //Reiniciar el state
         setExpense({
