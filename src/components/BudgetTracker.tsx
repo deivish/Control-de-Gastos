ff637f4ed63acb4a7cbed408a3665878492b90ef
deivis herrera <herrera.15cortes@gmail.com>
2025-08-26 00:07:10 -0500

    component created with quantities and graph

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,8 +1,35 @@
+import AmountDisplay from "./AmountDisplay";
 
 export default function BudgetTracker() {
   return (
-    <div>
-        BudgetTracker
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="felx justify-center">
+            <img src="/grafico.jpg" alt="Gráfica de gastos" />
+        </div>
+
+        <div className="felx flex-col justify-center items-center gap-8">
+            <button
+                type="button"
+                className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+            >
+                Resetear App
+            </button>
+
+            <AmountDisplay
+                label="Presupuesto"
+                amount= {300}
+            />
+
+            <AmountDisplay
+                label="Disponible"
+                amount= {200}
+            />
+
+            <AmountDisplay
+                label="Gastado"
+                amount= {100}
+            />
+        </div>
     </div>
   )
 }
